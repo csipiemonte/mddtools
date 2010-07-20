f7f88410f3c73849c8d764764cee1e035d4cf19e
CAPITANI Simone 1681 <simone.capitani@csi.it>
2010-07-20 13:18:14 +0000

    [SC] introdotta gestione su thread separato di invio informazioni a mddtracking
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/mddtools/trunk@1804 be04b910-fd5c-0410-891f-bca81b977169

--- src/mddtools/usagetracking/TrackingSender.java
+++ src/mddtools/usagetracking/TrackingSender.java
@@ -2,40 +2,44 @@ package mddtools.usagetracking;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Enumeration;
 import java.util.Properties;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 
 public class TrackingSender {
 
 	public static void sendTrackingInfo(Properties info){
 		String url = getTrackingServiceURL();
-		HttpClient client = new HttpClient();
-		PostMethod method = new PostMethod(url);
+		final HttpClient client = new HttpClient();
+		final PostMethod method = new PostMethod(url);
 		Enumeration keys= info.keys();
 		while(keys.hasMoreElements()){
 			String currKey = (String)keys.nextElement();
 			String currVal = (String)info.getProperty(currKey);
 			method.addParameter(currKey, currVal);
 		}
-		method.getParams().setSoTimeout(2000);
-		try {
-			int code = client.executeMethod(method);
-			System.out.println("tracking response code:"+code);
-		} catch (HttpException e) {
-			System.out.println("Errore invio info tracking"+e);
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Errore invio info tracking:"+e);
-			e.printStackTrace();
-		}
-		
+		method.getParams().setSoTimeout(150000);
+		Thread t = new Thread("TrackingSenderThread") {
+		  int code = -1;
+		  public void run() {
+			  while (code==-1) {
+				try {
+		        	code = client.executeMethod(method);	
+		        	System.out.println("Tracking response code: "+code);
+				} catch (HttpException e) {
+					System.out.println("Errore invio info tracking: "+e);
+					e.printStackTrace();
+				} catch (IOException e) {
+					System.out.println("Errore invio info tracking: "+e);
+					e.printStackTrace();
+				}						
+	         }			  
+	      }			      
+		};
+		t.start();		
 	}
 	
 	private static String getTrackingServiceURL(){
